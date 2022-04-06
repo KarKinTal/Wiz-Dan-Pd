e6948d0853a7b6eacac807337b27a2242f8e1178
KarKinTal <100282563+KarKinTal@users.noreply.github.com>
2022-04-06 12:59:02 +0200

    Add files via upload

---
+++ pd7/main.py
@@ -0,0 +1,18 @@
+import numpy as np
+# # Zadanie 1
+# zad1 = np.arange(4, 81, 4)
+# print(zad1)
+
+# # Zadanie 2
+# zad2 = np.arange(5, dtype = 'float64')
+# print(zad2)
+# print(zad2.dtype)
+# zad2_k = zad2.astype('int')
+# print(zad2_k)
+# print(zad2_k.dtype)
+
+# # Zadanie 3
+# def zadanie3 (n=3)
+#     return np.array(4, 81, 4)
+# n = input('Podaj n: ')
+# n = int(n)
